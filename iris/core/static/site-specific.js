6fbf64a3041774ee00cc43cecdd7f62a2c2038b7
Aleksi Häkli <aleksi.hakli@intel.com>
2014-04-16 12:19:28 +0300

    Added group creation view to submissions
    REs have a selection view for submissions for a product
    and general Tizen submissions they can group now.
    
    This change is heavily interdependant, hence the size.
    This change also connects URLs to basic CRUD views,
    which is necessary for having a working state.
    
    Change-Id: I9bfa0b7eadccc6d2703307d9f4bde033d10e3f1a
    Signed-off-by: Aleksi Häkli <aleksi.hakli@intel.com>

--- iris/core/static/site-specific.js
+++ iris/core/static/site-specific.js
@@ -13,9 +13,31 @@
  */
 
 /**
+ * Utility functions
+ * */
+
+var findIndex = function(list, value) {
+    for (var index in list) {
+        if (list[index] === value) {
+            return index;
+        }
+    }
+
+    return -1;
+};
+
+/**
  * Attaches filtering to the submissions summary table.
  */
 $(document).ready(function() {
+    if (!$('#submissions-summary').length) {
+        return;
+    }
+
+    // Glyphicon constants for the BootStrap manipulation
+    var up = 'glyphicon-chevron-up';
+    var down = 'glyphicon-chevron-down';
+
     // Pagination is disabled before it's styled for BootStrap 3
     $('#submissions-summary').dataTable({
         'bPaginate': false,
@@ -25,23 +47,31 @@ $(document).ready(function() {
     $('#submissions-summary_filter').addClass('hidden');
     $('#submissions-summary_info').addClass('hidden');
 
-    $("#date-filter").keyup(function() {
+    // Global column filtering
+    $('#global-filter').keyup(function() {
         $('#submissions-summary').dataTable().fnFilter(
-            $("#date-filter").val(), 0, true);
+            $('#global-filter').val(), null, true);
     });
 
-    $("#name-filter").keyup(function() {
-        $('#submissions-summary').dataTable().fnFilter(
-            $("#name-filter").val(), 1, true);
+    $('#product-filter').change(function(event) {
+        var appUrl = '/app/submissions/summary/';
+        productName = event.target.value;
+
+        if (productName) {
+            $(location).attr('href', appUrl + '?product=' + productName)
+        } else {
+            $(location).attr('href', appUrl)
+        }
     });
 
-    $("#tree-submission-filter").keyup(function() {
+    // Subfilters for each table column
+    $('#name-filter').keyup(function() {
         $('#submissions-summary').dataTable().fnFilter(
-            $("#tree-submission-filter").val(), 2, true);
+            $('#name-filter').val(), 0, true);
     });
 
-    $("#status-filter").change(function() {
-        var key = $("#status-filter").val();
+    $('#status-filter').change(function() {
+        var key = $('#status-filter').val();
         var filtermap = {
             'Any status': '',
             'Pending': '^((?!(ACCEPTED|REJECTED)).)*$',
@@ -53,6 +83,141 @@ $(document).ready(function() {
         };
 
         $('#submissions-summary').dataTable().fnFilter(
-            filtermap[key], 3, true);
+            filtermap[key], 1, true);
+    });
+
+    $('#tree-submission-filter').keyup(function() {
+        $('#submissions-summary').dataTable().fnFilter(
+            $('#tree-submission-filter').val(), 2, true);
+    });
+
+    $('#created-date-filter').keyup(function() {
+        $('#submissions-summary').dataTable().fnFilter(
+            $('#created-date-filter').val(), 3, true);
+    });
+
+    $('#updated-date-filter').keyup(function() {
+        $('#submissions-summary').dataTable().fnFilter(
+            $('#updated-date-filter').val(), 4, true);
+    });
+
+    // React to dropdowns and sorting visually with chevrons
+    $('#submissions-column-toggle').click(function() {
+        var chevron = $('#submissions-filter-toggle-chevron');
+
+        if (chevron.hasClass(down)) {
+            chevron.removeClass(down).addClass(up);
+        } else {
+            chevron.removeClass(up).addClass(down);
+        }
+    });
+
+    $('.sorter').click(function (event) {
+        // Clear previous chevrons from column headers
+        $('.sorter').children('span').removeClass(up).removeClass(down);
+
+        // Add new chevrons to the table header that was clicked
+        var th = $(event.target)
+        var span = $(th.children('span'));
+
+        if (span.hasClass('glyphicon')) {
+            if (th.hasClass('sorting_asc')) {
+                span.removeClass(up).addClass(down);
+            } else if (th.hasClass('sorting_desc')) {
+                span.removeClass(down).addClass(up);
+            }
+        }
+    });
+
+    // By default, hide accepted and rejected submissions from the user
+    $('#submissions-summary').dataTable().fnFilter(
+        '^((?!(ACCEPTED|REJECTED)).)*$', 1, true);
+
+    // Sort the table by 'Updated' column
+    $('#submissions-summary').dataTable().fnSort([[4, 'asc']]);
+    $('.sorter:contains("Updated")').children('span').addClass(down);
+
+    // Listener for submission selection checkboxes
+});
+
+/**
+ * Submissions grouping page helpers
+ */
+$(document).ready(function() {
+    var selectedSubmissions = []
+
+    window.selectSubmission = function (id) {
+        var i = findIndex(selectedSubmissions, id);
+
+        if (i !== -1) {
+            selectedSubmissions.splice(i, 1);
+
+            if (selectedSubmissions.length === 0) {
+                $('#create-group-submit').prop('disabled', true)
+            }
+        } else {
+            selectedSubmissions.push(id);
+
+            if (selectedSubmissions.length > 0) {
+                $('#create-group-submit').prop('disabled', false)
+            }
+        }
+    }
+
+    var timeString = function () {
+        var date = new Date();
+        var Y = date.getUTCFullYear();
+        var m = date.getUTCMonth() + 1;
+        var d = date.getUTCDate();
+        var H = date.getUTCHours();
+        var M = date.getUTCMinutes();
+        var S = date.getUTCSeconds();
+
+        // Ensure we have padded values
+        H = H < 10 ? '0' + H : H;
+        S = S < 10 ? '0' + S : S;
+        m = m < 10 ? '0' + m : m;
+
+        return Y + m + d + '.' + H + M + S;
+    };
+
+    var setSubmitTime = function () {
+        $('#submit-time').html(timeString());
+    }
+
+    setSubmitTime();
+    window.setInterval(setSubmitTime, 1000);
+
+    $('#create-group-submit').click(function (event) {
+        // Don't go to URL; create an AJAX request instead
+        event.preventDefault();
+
+        // Get form fields, add submission list to them.
+        var form = $('#create-group-form');
+        var form_data = form.serializeArray();
+
+        form_data.push({
+            name: 'submissions',
+            value: selectedSubmissions.join(',')
+        });
+
+        // Formulate an AJAX request, send asynchronously
+        var request = {
+            type: form.attr('method'),
+            url: form.attr('action'),
+            data: $.param(form_data),  // serialize form data for backend
+            success: function(data) {
+                $('.submissions-select').prop('checked', false);
+                selectedSubmissions = [];
+                var group_url = '/app/submissions/submissiongroups/';
+                window.location = group_url + data.product;
+            },
+            error: function(data) {
+                console.log('Failed at creating a new submission group');
+                console.log('Sent data: ', JSON.stringify(form_data));
+            }
+        };
+
+        $.ajax(request);
     });
 });
--- iris/submissions/urls.py
+++ iris/submissions/urls.py
@@ -23,4 +23,44 @@ urlpatterns = patterns(
     'iris.submissions.views.base',
     url(r'^(?i)$', 'index', name='index'),
     url(r'^summary/$', 'summary', name='summary'),
+    url(r'^group/$', 'create_group', name='create_group'),
+    url(r'^group/create/$', 'create_group_ajax', name='create_group_ajax'),
+)
+
+urlpatterns += patterns(
+    'iris.submissions.views.create',
+    url(r'^submissiongroups/create/$',
+        'submissiongroups', name='create_submissiongroups'),
+)
+
+urlpatterns += patterns(
+    'iris.submissions.views.read',
+    url(r'^submissions(?:/(?P<pkid>\d+))?/$',
+        'submissions', name='read_submissions'),
+    url(r'^submissiongroups(?:/(?P<pkid>\d+))?/$',
+        'submissiongroups', name='read_submissiongroups'),
+)
+
+urlpatterns += patterns(
+    'iris.submissions.views.update',
+    url(r'^submissions(?:/(?P<pkid>\d+))?/update/$',
+        'submissions', name='update_submissions'),
+    url(r'^submissions(?:/(?P<pkid>\d+))?/accept/$',
+        'accept_submissions', name='accept_submissions'),
+    url(r'^submissions(?:/(?P<pkid>\d+))?/reject/$',
+        'reject_submissions', name='reject_submissions'),
+    url(r'^submissiongroups(?:/(?P<pkid>\d+))?/update/$',
+        'submissiongroups', name='update_submissiongroups'),
+    url(r'^submissiongroups(?:/(?P<pkid>\d+))?/accept/$',
+        'accept_submissiongroups', name='accept_submissiongroups'),
+    url(r'^submissiongroup(?:/(?P<pkid>\d+))?/reject/$',
+        'reject_submissiongroups', name='reject_submissiongroups'),
+)
+
+urlpatterns += patterns(
+    'iris.submissions.views.delete',
+    url(r'^submissions(?:/(?P<pkid>\d+))?/delete/$',
+        'submissions', name='delete_submissions'),
+    url(r'^submissiongroups(?:/(?P<pkid>\d+))?/delete/$',
+        'submissiongroups', name='delete_submissiongroups'),
 )
