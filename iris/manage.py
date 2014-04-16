dc85b90e0590e2aee6619071e4f4d988ed8b46a1
Aleksi Häkli <aleksi.hakli@intel.com>
2014-04-16 12:28:54 +0300

    Added improved path resolving to manage.py module
    Dev folder now resolves as the primary source for IRIS files in the project.
    
    Fixes some virtual environment problems with reloading.
    
    Change-Id: Ib30879d50fb05827f388afe686d4b0999e3c8850
    Signed-off-by: Aleksi Häkli <aleksi.hakli@intel.com>

--- iris/manage.py
+++ iris/manage.py
@@ -12,10 +12,16 @@
 This is the management module for the iris-core project.
 """
 
+from os.path import abspath, join, dirname
 from os import environ
-from sys import argv
+from sys import argv, path
 
 if __name__ == "__main__":
+    PROJECT_ROOT = abspath(join(dirname(__file__), '..'))
+
+    if PROJECT_ROOT not in path:
+        path.insert(0, PROJECT_ROOT)
+
     environ.setdefault("DJANGO_SETTINGS_MODULE", "iris.core.settings")
 
     from django.core.management import execute_from_command_line
