8fcced16322bc2ab80a69d2b943653724bf57ea7
Huang Hao <hao.h.huang@intel.com>
2014-06-09 15:33:10 +0800

    Common function to show user's display name
    Add a template tag "user_display_name" to show user's display
    name.
    
    Although we have user profile class, however in many place we
    don't use profile but the origin user instance instead. So this
    common function can't be put into profile class.
    
    Fixes: 1918
    Change-Id: I02bdc4b797b85b54ce9d5d76c5b1f13b82788dab

---
+++ iris/core/templatetags/user_display_name.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def user_display_name(user):
+    names = []
+    if user.last_name:
+        names.append(user.last_name)
+    if user.first_name:
+        names.append(user.first_name)
+    if names:
+        return ', '.join(names)
+    return user.username
