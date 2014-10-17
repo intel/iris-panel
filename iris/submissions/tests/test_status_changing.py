2a54b4240a2b74316574988a6fabcc8b466a0ca2
Huang Hao <hao.h.huang@intel.com>
2014-10-17 16:13:23 +0800

    Refactor test_status_changing.py
    Move common code into setUp().
    
    Add method package_built(), image_building(), image_created() to
    send events for testing. It makes case code more readable.
    
    Change-Id: Ic47ac3cb62940166eefc0ed694bca470b9b04bd2

--- iris/submissions/tests/test_status_changing.py
+++ iris/submissions/tests/test_status_changing.py
@@ -12,101 +12,84 @@ class StatusTest(TestCase):
     fixtures = ['users', 'domains', 'subdomains', 'gittrees', 'products', 'submissions']
     url = '/api/submissions/events/%s/'
 
+    gitpath = 'framework/system/dlog'
+    tag = 'submit/trunk/yyyy-mm-dd'
+    project = 'home:prerelease:tizen:ivi:submit:trunk:01'
+
+    def setUp(self):
+        """
+        Create a submission tag and pre-release project
+        """
+        self.login()
+        self.client.post(self.url % 'submitted', {
+                'gitpath': self.gitpath,
+                'tag': self.tag,
+                'commit_id': 'sha1',
+                'submitter_email': 'someone@localhost',
+                })
+        self.client.post(self.url % 'pre_created', {
+                'gitpath': self.gitpath,
+                'tag': self.tag,
+                'product': 'Tizen:IVI',
+                'project': self.project,
+                })
+
     def login(self, user='robot', pwd='robot'):
         assert self.client.login(username=user, password=pwd)
+
+    def package_built(self, package, is_success):
+        """
+        Send a package_built event for `package` with status `is_success`
+        """
+        self.client.post(self.url % 'package_built', {
+                'name': package,
+                'repo': 'standard',
+                'arch': 'armv7el',
+                'project': self.project,
+                'status': 'OBS_BUILD_SUCCESS' if is_success else 'OBS_BUILD_FAIL',
+                'repo_server': 'http://build.server',
+                })
+
+    def image_building(self, image):
+        """
+        Send a image_building event for `image`
+        """
+        self.client.post(self.url % 'image_building', {
+                'name': image,
+                'project': self.project,
+                'repo': 'standard',
+                })
+
+    def image_created(self, image, is_success):
+        """
+        Send a image_created event for `image` with status `is_success`
+        """
+        self.client.post(self.url % 'image_created', {
+                'name': image,
+                'project': self.project,
+                'status': 'success' if is_success else 'failed',
+                'url': 'http://url1.com',
+                })
+
+    @property
+    def submission(self):
+        return Submission.objects.get(
+            name=self.tag, gittree__gitpath=self.gitpath)
 
     def test_package_failed(self):
         """
         One package succeed and the other failed, then submission status will be "Package Failed"
         """
-        gitpath = 'framework/system/dlog'
-        tag = 'submit/trunk/yyyy-mm-dd'
-        project = 'home:prerelease:tizen:ivi:submit:trunk:01'
-        self.login()
-        self.client.post(self.url % 'submitted', {
-                'gitpath': gitpath,
-                'tag': tag,
-                'commit_id': 'sha1',
-                'submitter_email': 'someone@localhost',
-                })
-        self.client.post(self.url % 'pre_created', {
-                'gitpath': gitpath,
-                'tag': tag,
-                'product': 'Tizen:IVI',
-                'project': project,
-                })
+        self.package_built('dlog-api', False)
+        self.package_built('dlog', True)
 
-        self.client.post(self.url % 'package_built', {
-                'name': 'dlog-api',
-                'repo': 'standard',
-                'arch': 'armv7el',
-                'project': project,
-                'status': 'OBS_BUILD_FAIL',
-                'repo_server': 'http://build.server',
-                })
-
-        self.client.post(self.url % 'package_built', {
-                'name': 'dlog',
-                'repo': 'standard',
-                'arch': 'i586',
-                'project': project,
-                'status': 'OBS_BUILD_SUCCESS',
-                'repo_server': 'http://build.server',
-                })
-
-        group = BuildGroup.objects.get(name=project)
-        submission = Submission.objects.get(
-            name=tag, gittree__gitpath=gitpath)
-        self.assertEquals('15_PKGFAILED', group.status)
-        self.assertEquals('15_PKGFAILED', submission.status)
-
+        self.assertEquals('15_PKGFAILED', self.submission.status)
 
     def test_image_failed(self):
-        gitpath = 'framework/system/dlog'
-        tag = 'submit/trunk/yyyy-mm-dd'
-        project = 'home:prerelease:tizen:ivi:submit:trunk:01'
-        self.login()
-        self.client.post(self.url % 'submitted', {
-                'gitpath': gitpath,
-                'tag': tag,
-                'commit_id': 'sha1',
-                'submitter_email': 'someone@localhost',
-                })
-        self.client.post(self.url % 'pre_created', {
-                'gitpath': gitpath,
-                'tag': tag,
-                'product': 'Tizen:IVI',
-                'project': project,
-                })
+        self.image_building('image1')
+        self.image_building('image2')
 
-        self.client.post(self.url % 'image_building', {
-                'name': 'image1',
-                'project': project,
-                'repo': 'standard',
-                })
+        self.image_created('image1', True)
+        self.image_created('image2', False)
 
-        self.client.post(self.url % 'image_building', {
-                'name': 'image2',
-                'project': project,
-                'repo': 'standard',
-                })
-
-        self.client.post(self.url % 'image_created', {
-                'name': 'image1',
-                'project': project,
-                'status': 'success',
-                'url': 'http://url1.com',
-                })
-
-        self.client.post(self.url % 'image_created', {
-                'name': 'image2',
-                'project': project,
-                'status': 'failed',
-                'url': 'http://url2.com',
-                })
-
-        group = BuildGroup.objects.get(name=project)
-        submission = Submission.objects.get(
-            name=tag, gittree__gitpath=gitpath)
-        self.assertEquals('25_IMGFAILED', group.status)
-        self.assertEquals('25_IMGFAILED', submission.status)
+        self.assertEquals('25_IMGFAILED', self.submission.status)
