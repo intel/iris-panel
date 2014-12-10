7491ff10b342f5afab513d9b15e544052e25381b
Huanhuan Li <huanhuanx.li@intel.com>
2014-12-10 13:13:56 +0800

    Delete unuseful import
    Change-Id: I0eeda5376bf3a46141b94746d03691216fdd31de
    refs: #2229

--- iris/packagedb/views/read.py
+++ iris/packagedb/views/read.py
@@ -20,7 +20,7 @@ from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 
 from iris.core.models import (Domain, SubDomain, License, GitTree, Package,
-        Product, Image, Snapshot)
+        Product, Image)
 from iris.packagedb.injectors import (inject_domain, inject_subdomain,
         inject_gittree)
 
